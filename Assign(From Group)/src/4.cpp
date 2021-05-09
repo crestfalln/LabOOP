7df967ae071f2255cda7bdf012c11fd8ea8298d3
crestfalln <guptahman01@gmail.com>
2021-05-09 20:41:20 +0530

    Revert "Revert "Restructuring and Another Assigment(not completed yet)""
    This reverts commit d98b7fbf3982739af71703bec5b19909d07ca16b.
    I accidently deleted everything with the previous revert

---
+++ Assign(From Group)/src/4.cpp
@@ -0,0 +1,18 @@
+//QUESTION An election is contested by five candidates. The candidates are numbered 1 to 5 and the voting is done by marking the candidate number on the ballot paper. Write a program to read the ballots and count the vote cast for each candidate using an array variable count. In case, a number read is outside the range 1 to 5, the ballot should be considered as a “spoilt ballot” and the program should also count the numbers of “spoilt ballots”.
+
+#include <iostream>
+#include <array>
+
+int main()
+{
+    std::array<int , 5> count;
+    int spoiltVotes;
+    for(int i; std::cin >> i; )
+    {
+        if(i > 5 || i < 1)
+            spoiltVotes++;
+        else
+            count[i-1]++;
+    }
+    
+}
